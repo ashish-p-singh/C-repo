02223e98ebb97386afbccce04090d4b7f67d6263
ashish-p-singh <ashishpratap.singh@outlook.com>
2026-02-04 22:34:51 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+#include <stdbool.h>
+int main(){
+    //hi
+    /*
+    h
+    i
+    */
+   int age=5;
+   float lol=3.134;
+   double big=2.3623856385683;
+   char grade='A';
+   char name[]="John Doe";
+   bool isOnline=true;
+    printf("Hello, World!\n");
+    printf("This is a test C program.\n");
+    printf("My age is %d\n",age);
+    printf("float is %.2f\n",lol);
+    printf("double is %.12lf\n",big);
+    printf("grade is %c\n",grade);
+    printf("name is %s\n",name);
+    printf("%d\n",isOnline);
+    return 0;   
+}
